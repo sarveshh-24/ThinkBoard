e453ed0435ede4815b28a5f9314832bc2ae180f4
sarveshh-24 <yadavkumarsarvesh24@gmail.com>
2026-08-02 20:29:17 +0000

    Second

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import express from "express"
 
 const app = express()
 
+app.get("/api/notes", (req,res) => {
+    res.status(200).send("You got 0 notes");
+});
+
 app.listen(5001, () =>{
     console.log("Server started on PORT : 5001")
 })
