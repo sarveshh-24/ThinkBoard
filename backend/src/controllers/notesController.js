db1fbbf5adbffc949847eb57131cdc7d2e5e6116
sarveshh-24 <yadavkumarsarvesh24@gmail.com>
2026-08-03 19:58:39 +0000

    5th --> Controller-->Create Notes

--- backend/src/controllers/notesController.js
+++ backend/src/controllers/notesController.js
@@ -3,7 +3,7 @@ import Note from "../models/Note.js";
 
 async function getAllNotes (req, res) {
     try {
-        const Note = await Note.find();
+        const notes = await Note.find();
         res.status(200).json(notes);
     } catch (error) {
         console.error("Error in getAllNotes controller ", error);
@@ -11,15 +11,24 @@ async function getAllNotes (req, res) {
     }
 }
 
-const createNote = (req, res) => {
-    res.status(201).json({message: "Note created successfully"});
+async function createNote (req, res) {
+    try {
+        const {title, content} = req.body
+        const newNote = new Note({title, content});
+
+        await newNote.save();
+        res.status(201).json({message : "New Note successfully created"});
+    } catch (error) {
+        console.error("Error in createNote controller ", error);
+        res.status(500).json({message : "Internal Server Error"});
+    }
 }
 
-const updateNote = (req, res) => {
+async function updateNote (req, res) {
     res.status(200).json({message: "Note updated successfully"});
 }
 
-const deleteNote = (req, res) => {
+async function deleteNote (req, res) {
     res.status(200).json({message: "Note deleted successfully"});
 }
 
