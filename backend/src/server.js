9500c6a83c7cbe038d278e0e5a22d63090308416
sarveshh-24 <yadavkumarsarvesh24@gmail.com>
2026-09-06 16:28:24 +0530

    Set trust proxy for Express app

--- backend/src/server.js
+++ backend/src/server.js
@@ -12,6 +12,8 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config();
 
 const app = express();
+
+app.set("trust proxy", 1);
 const PORT = process.env.PORT || 5001;
 const __dirname = path.resolve();
 
